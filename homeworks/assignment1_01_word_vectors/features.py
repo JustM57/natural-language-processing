c955ff89f99949e777a073278c9e9c5e5a33c363
JustM57 <tiir57@yandex.ru>
2022-03-27 22:46:56 +0300

    2 homeworks

--- homeworks/assignment1_01_word_vectors/features.py
+++ homeworks/assignment1_01_word_vectors/features.py
@@ -1,7 +1,8 @@
-from collections import OrderedDict
+from collections import OrderedDict, Counter
 from sklearn.base import TransformerMixin
 from typing import List, Union
 import numpy as np
+import pandas as pd
 
 
 class BoW(TransformerMixin):
@@ -28,7 +29,11 @@ class BoW(TransformerMixin):
         # task: find up to self.k most frequent tokens in texts_train,
         # sort them by number of occurences (highest first)
         # store most frequent tokens in self.bow
-        raise NotImplementedError
+        c = Counter()
+        for text in X:
+            c.update(text.split())
+        c = c.most_common(self.k)
+        self.bow = pd.Series(np.arange(self.k), index=[key for key, val in c])
 
         # fit method must always return self
         return self
@@ -39,10 +44,12 @@ class BoW(TransformerMixin):
         :param text: text to be transformed
         :return bow_feature: feature vector, made by bag of words
         """
-
-        result = None
-        raise NotImplementedError
-        return np.array(result, "float32")
+        words = Counter(text.split())
+        words = pd.Series({key: val for key, val in words.items() if key in self.bow.index})
+        idx = self.bow[words.index]
+        vector = np.zeros(self.k)
+        vector[idx] = words
+        return vector
 
     def transform(self, X: np.ndarray, y=None) -> np.ndarray:
         """
@@ -53,7 +60,7 @@ class BoW(TransformerMixin):
         return np.stack([self._text_to_bow(text) for text in X])
 
     def get_vocabulary(self) -> Union[List[str], None]:
-        return self.bow
+        return self.bow.index.tolist()
 
 
 class TfIdf(TransformerMixin):
@@ -80,8 +87,11 @@ class TfIdf(TransformerMixin):
         """
         :param X: array of texts to be trained on
         """
-        raise NotImplementedError
-
+        c = Counter()
+        for text in X:
+            c.update(set(text.split()))
+        c = c.most_common(self.k)
+        self.idf = OrderedDict({key: (idx, np.log(X.shape[0] / val)) for idx, (key, val) in enumerate(c)})
         # fit method must always return self
         return self
 
@@ -92,10 +102,15 @@ class TfIdf(TransformerMixin):
         :param text: text to be transformed
         :return tf_idf: tf-idf features
         """
+        words = Counter(text.split())
+        words = pd.Series({key: val * self.idf[key][1] for key, val in words.items() if key in self.idf.keys()})
+        idx = [self.idf[key][0] for key in words.index]
+        vector = np.zeros(self.k)
+        vector[idx] = words.values
+        if self.normalize:
+            vector /= (vector.sum() + 1e-8)
+        return vector
 
-        result = None
-        raise NotImplementedError
-        return np.array(result, "float32")
 
     def transform(self, X: np.ndarray, y=None) -> np.ndarray:
         """
@@ -103,4 +118,4 @@ class TfIdf(TransformerMixin):
         :return: array of transformed texts
         """
         assert self.idf is not None
-        return np.stack([self._text_to_tf_idf(text) for text in X])
+        return np.array([self._text_to_tf_idf(text) for text in X])
